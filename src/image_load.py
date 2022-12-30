0b9d61a22630d5ce426d3a2fcaacd572f2e817ea
RyanWerth <ryandwerth@gmail.com>
2022-12-30 13:24:46 -0800

    built app

---
+++ src/image_load.py
@@ -0,0 +1,69 @@
+import PIL.Image
+import PIL.ImageOps
+import math
+import numpy as np
+
+def exif_transpose(img):
+    if not img:
+        return img
+
+    exif_orientation_tag = 274
+
+    # Check for EXIF data (only present on some files)
+    if hasattr(img, "_getexif") and isinstance(img._getexif(), dict) and exif_orientation_tag in img._getexif():
+        exif_data = img._getexif()
+        orientation = exif_data[exif_orientation_tag]
+
+        # Handle EXIF Orientation
+        if orientation == 1:
+            # Normal image - nothing to do!
+            pass
+        elif orientation == 2:
+            # Mirrored left to right
+            img = img.transpose(PIL.Image.FLIP_LEFT_RIGHT)
+        elif orientation == 3:
+            # Rotated 180 degrees
+            img = img.rotate(180)
+        elif orientation == 4:
+            # Mirrored top to bottom
+            img = img.rotate(180).transpose(PIL.Image.FLIP_LEFT_RIGHT)
+        elif orientation == 5:
+            # Mirrored along top-left diagonal
+            img = img.rotate(-90, expand=True).transpose(PIL.Image.FLIP_LEFT_RIGHT)
+        elif orientation == 6:
+            # Rotated 90 degrees
+            img = img.rotate(-90, expand=True)
+        elif orientation == 7:
+            # Mirrored along top-right diagonal
+            img = img.rotate(90, expand=True).transpose(PIL.Image.FLIP_LEFT_RIGHT)
+        elif orientation == 8:
+            # Rotated 270 degrees
+            img = img.rotate(90, expand=True)
+
+    return img
+
+
+def transform_image(img, mode='RGB'):
+    # Load the image with PIL
+    # img = PIL.Image.open(file)
+
+    if hasattr(PIL.ImageOps, 'exif_transpose'):
+        # Very recent versions of PIL can do exit transpose internally
+        img = PIL.ImageOps.exif_transpose(img)
+    else:
+        # Otherwise, do the exif transpose ourselves
+        img = exif_transpose(img)
+
+    width, height = img.size
+
+    if width > 1000 or height > 1000:
+      if width > height:
+        new_size = (1000,math.floor((height/width)*1000))
+      else:
+        new_size = (math.floor((width/height)*1000),1000)
+    
+      img = img.resize(new_size)
+
+    img = img.convert(mode)
+
+    return np.array(img)
---
+++ src/predictions.py
@@ -0,0 +1,105 @@
+import numpy as np
+import cv2
+import pytesseract as pt
+
+
+INPUT_WIDTH = 640
+INPUT_HEIGHT = 640
+
+def get_detections(img,net):
+    # 1.CONVERT IMAGE TO YOLO FORMAT
+    image = img.copy()
+    row, col, d = image.shape
+
+    max_rc = max(row,col)
+    input_image = np.zeros((max_rc,max_rc,3),dtype=np.uint8)
+    input_image[0:row,0:col] = image
+
+    # 2. GET PREDICTION FROM YOLO MODEL
+    blob = cv2.dnn.blobFromImage(input_image,1/255,(INPUT_WIDTH,INPUT_HEIGHT),swapRB=True,crop=False)
+    net.setInput(blob)
+    preds = net.forward()
+    detections = preds[0]
+    
+    return input_image, detections
+
+def non_maximum_supression(input_image,detections):
+    
+    # 3. FILTER DETECTIONS BASED ON CONFIDENCE AND PROBABILIY SCORE
+    
+    # center x, center y, w , h, conf, proba
+    boxes = []
+    confidences = []
+
+    image_w, image_h = input_image.shape[:2]
+    x_factor = image_w/INPUT_WIDTH
+    y_factor = image_h/INPUT_HEIGHT
+
+    for i in range(len(detections)):
+        row = detections[i]
+        confidence = row[4] # confidence of detecting license plate
+        if confidence > 0.4:
+            class_score = row[5] # probability score of license plate
+            if class_score > 0.25:
+                cx, cy , w, h = row[0:4]
+
+                left = int((cx - 0.5*w)*x_factor)
+                top = int((cy-0.5*h)*y_factor)
+                width = int(w*x_factor)
+                height = int(h*y_factor)
+                box = np.array([left,top,width,height])
+
+                confidences.append(confidence)
+                boxes.append(box)
+
+    # 4.1 CLEAN
+    boxes_np = np.array(boxes).tolist()
+    confidences_np = np.array(confidences).tolist()
+    
+    # 4.2 NMS
+    index = cv2.dnn.NMSBoxes(boxes_np,confidences_np,0.25,0.45)
+    
+    return boxes_np, confidences_np, index
+
+# extrating text
+def extract_text(image,bbox):
+    x,y,w,h = bbox
+    roi = image[y:y+h, x:x+w]
+    
+    if 0 in roi.shape:
+        return 'no number'
+    
+    else:
+        text = pt.image_to_string(roi)
+        text = text.strip()
+        
+        return {"image": roi, "text": text}
+
+def drawings(image,boxes_np,confidences_np,index):
+    # 5. Drawings
+    highest_conf = 0
+    highest_conf_plate_text = ''
+    highest_conf_plate_image = []
+    for ind in index:
+        x,y,w,h =  boxes_np[ind]
+        bb_conf = confidences_np[ind]
+        conf_text = 'plate: {:.0f}%'.format(bb_conf*100)
+        license_text_dict = extract_text(image,boxes_np[ind])
+        license_text = license_text_dict["text"]
+        license_text_img = license_text_dict["image"]
+
+        if bb_conf > highest_conf:
+            highest_conf = bb_conf
+            highest_conf_plate_text = license_text
+            highest_conf_plate_image = license_text_img
+
+
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,255),2)
+        cv2.rectangle(image,(x,y-40),(x+w,y),(255,0,255),-1)
+        cv2.rectangle(image,(x,y+h),(x+w,y+h+40),(0,0,0),-1)
+
+
+        cv2.putText(image,conf_text,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),2)
+        cv2.putText(image,license_text,(x,y+h+27),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+
+    return {"full_image":image, "plate_img": highest_conf_plate_image, "text": highest_conf_plate_text, "conf": highest_conf}
