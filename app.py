a62d7dbe21b28c20a53f1fbb6b3ed376f4712b4a
RyanWerth <ryandwerth@gmail.com>
2022-12-30 14:20:02 -0800

    added words and ballonons

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import streamlit as st
 
 
-def main():
-
-    st.title('License Plate Detection')
-
 
 if __name__ == '__main__':
     st.set_page_config(page_title="Main Page", page_icon="")
 
-    main()
+    st.title('License Plate Detection')
+
+    st.markdown('Using **Yolov5** trained a model to identify license plates in an image')
+    st.markdown('Using **Pytesseract** I extract the text from the license plate')
+    st.markdown("""
+    Try it yourself. Navigate to either the picture capture or upload page on the right.
+    """)
+
+    balls = st.button("I'm Just Here For Balloons")
+    if balls:
+        st.balloons()
+
